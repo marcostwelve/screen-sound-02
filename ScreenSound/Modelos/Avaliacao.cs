5dad5cc34184036576a5e78d58337004ce3aff56
marcostwelve <marcostwelve>
2023-09-04 13:38:32 -0300

    new ss v1

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,24 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        if(nota > 10)
+        {
+            nota = 10;
+        } else if(nota < 0)
+        {
+            nota = 0;
+        }
+        return new Avaliacao(nota);
+    }
+}
